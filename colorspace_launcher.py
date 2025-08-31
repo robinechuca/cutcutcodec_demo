5ce8bc0443b306b3856a3936cc90a49c0c9be71c
robinechuca <serveurpython.oz@gmail.com>
2025-08-31 12:04:28 +0200

    Update colorspace_launcher.py

--- colorspace_launcher.py
+++ colorspace_launcher.py
@@ -1,4 +1,10 @@
+import pathlib
+import sys
 import urllib.request
 url = "https://framagit.org/robinechuca/cutcutcodec/-/raw/main/docs/demo/colorspace.py"
-urllib.request.urlretrieve(url, "colorspace_demo.py")
+file = pathlib.Path.cwd() / "colorspace_demo.py"
+if not file.exists():
+    urllib.request.urlretrieve(url, file)
+if sys.path[0] != str(file):
+    sys.path.insert(0, str(file))
 import colorspace_demo
